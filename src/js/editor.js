646bc6e101dacf8a79a55f50e5f5ed6fd71e4803
山雾 <129361010+hillmis@users.noreply.github.com>
2025-04-24 18:38:53 +0800

    Update editor.js

--- src/js/editor.js
+++ src/js/editor.js
@@ -34,7 +34,7 @@ class Editor {
     }
 
     async initializeMonaco() {
-        require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.40.0/min/vs' } });
+        require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.40.0/min/vs' } });
 
         await new Promise(resolve => {
             require(['vs/editor/editor.main'], () => {
